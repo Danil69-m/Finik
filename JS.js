7e36ecfc023a73792b2fddee4a0e5af3d0cae994
Danil69-m <danil.sementsev@mail.ru>
2024-10-01 21:57:15 +0500

    Сделал все задания на JS с png и pdf

---
+++ JS.js
@@ -0,0 +1,34 @@
+document.write(" Мороз и солнце; день чудесный! <br> Еще ты дремлешь, друг прелестный — <br>  Пора, красавица, проснись:<br> Открой сомкнуты негой взоры<br> Навстречу северной Авроры,<br> Звездою севера явись!<br> ...");
+
+
+const NAME  = 'Василий';
+const LOGIN = 'vasya123'; 
+const AGE = '18';
+const EMAIL = 'vasya123@mail.ru';
+
+// выведем значения переменных
+console.log("Исходные значения переменных");
+console.log('Имя: ' + NAME);
+console.log('Логин:' + LOGIN);
+console.log('Возраст:' + AGE);
+console.log('E-mail:' + EMAIL);
+
+// переопределим значения переменных
+let name = 'Анна';
+let login = 'anyta987';
+let age = '17';
+let email = 'anyta987@mail.ru';
+
+// выведем новые значения переменных
+console.log("Новые значения переменных");
+console.log('Имя: ' + name);
+console.log('Логин:' + login);
+console.log('Возраст:' + age);
+console.log('E-mail:' + email);
+
+
+
+
+
+
+
